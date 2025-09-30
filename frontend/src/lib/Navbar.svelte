0b874eaf694b9720bb9a8319c919d3389ac0b3d4
douglascdev <douglasc.dev@gmail.com>
2025-09-30 16:00:31 -0300

    make navbar a separate component

---
+++ frontend/src/lib/Navbar.svelte
@@ -0,0 +1,36 @@
+
+<div class="navbar bg-base-100 shadow-sm">
+	<div class="navbar-start">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+      </div>
+      <ul
+        tabindex="0"
+        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li><a>Homepage</a></li>
+        <li><a>Portfolio</a></li>
+        <li><a>About</a></li>
+      </ul>
+    </div>
+  </div>
+	<div class="flex-1">
+		<a class="btn btn-ghost text-xl" href="/">hashbot</a>
+	</div>
+	<!--
+  <button hidden id="login" class="btn" on:click={twitchRedirect()}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitch" viewBox="0 0 16 16">
+      <path d="M3.857 0 1 2.857v10.286h3.429V16l2.857-2.857H9.57L14.714 8V0zm9.714 7.429-2.285 2.285H9l-2 2v-2H4.429V1.143h9.142z"/>
+      <path d="M11.857 3.143h-1.143V6.57h1.143zm-3.143 0H7.571V6.57h1.143z"/>
+    </svg>
+    Login with Twitch
+  </button>
+  <button hidden id="logout" class="btn" on:click={twitchLogout()}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitch" viewBox="0 0 16 16">
+      <path d="M3.857 0 1 2.857v10.286h3.429V16l2.857-2.857H9.57L14.714 8V0zm9.714 7.429-2.285 2.285H9l-2 2v-2H4.429V1.143h9.142z"/>
+      <path d="M11.857 3.143h-1.143V6.57h1.143zm-3.143 0H7.571V6.57h1.143z"/>
+    </svg>
+    Logout
+  </button>
+		-->
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { onMount } from "svelte";
+	import Navbar from "$lib/Navbar.svelte";
 	let commands = [];
 	//	let data;
 
@@ -90,28 +91,7 @@
 	//	}
 </script>
 
-<div class="navbar bg-base-100 shadow-sm">
-	<div class="flex-1">
-		<a class="btn btn-ghost text-xl" href="/">hashbot</a>
-	</div>
-	<!--
-  <button hidden id="login" class="btn" on:click={twitchRedirect()}>
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitch" viewBox="0 0 16 16">
-      <path d="M3.857 0 1 2.857v10.286h3.429V16l2.857-2.857H9.57L14.714 8V0zm9.714 7.429-2.285 2.285H9l-2 2v-2H4.429V1.143h9.142z"/>
-      <path d="M11.857 3.143h-1.143V6.57h1.143zm-3.143 0H7.571V6.57h1.143z"/>
-    </svg>
-    Login with Twitch
-  </button>
-  <button hidden id="logout" class="btn" on:click={twitchLogout()}>
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitch" viewBox="0 0 16 16">
-      <path d="M3.857 0 1 2.857v10.286h3.429V16l2.857-2.857H9.57L14.714 8V0zm9.714 7.429-2.285 2.285H9l-2 2v-2H4.429V1.143h9.142z"/>
-      <path d="M11.857 3.143h-1.143V6.57h1.143zm-3.143 0H7.571V6.57h1.143z"/>
-    </svg>
-    Logout
-  </button>
-		-->
-</div>
-
+<Navbar />
 <div
 	class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100"
 >
