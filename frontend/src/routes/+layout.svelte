21545dc89c1b8663131bfbc55332bdcebcb78557
douglascdev <douglasc.dev@gmail.com>
2025-09-30 15:36:16 -0300

    add title

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import "../app.css";
+	import favicon from "$lib/assets/favicon.svg";
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>hashbot</title>
 </svelte:head>
 
 {@render children?.()}
