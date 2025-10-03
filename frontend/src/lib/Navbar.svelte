a59769489d7d4ff378768974923a5cd781e03abe
douglascdev <douglasc.dev@gmail.com>
2025-10-03 16:49:35 -0300

    translate navbar

--- frontend/src/lib/Navbar.svelte
+++ frontend/src/lib/Navbar.svelte
@@ -1,3 +1,7 @@
+<script>
+	import { m } from "./paraglide/messages";
+</script>
+
 <div class="navbar bg-base-100 shadow-sm">
 	<div class="navbar-start">
 		<a class="btn btn-ghost text-xl" href="/">hashbot</a>
@@ -6,22 +10,20 @@
 		<ul class="menu menu-horizontal px-1">
 			<li>
 				<details class="max-w-1xl">
-					<summary>Commands</summary>
+					<summary>{m.navbar_commands()}</summary>
 					<ul
 						class="p-2 z-10"
 						style="min-width: max-content;"
 					>
 						<li>
 							<a href="/commands"
-								>Full list of
-								commands</a
+								>{m.navbar_command_list()}</a
 							>
 						</li>
 						<li>
 							<a
 								href="/emote-management"
-								>Emote
-								management</a
+								>{m.navbar_emote_management()}</a
 							>
 						</li>
 					</ul>
